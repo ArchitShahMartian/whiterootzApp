dfcac17c47a264207fbedbde87d5c2181e7d9597
Archit Shah <archit0994@gmail.com>
2023-07-17 20:40:00 -0400

    added camera endpoint in frontend

--- whiterootz_react/src/services/api/Sensors.ts
+++ whiterootz_react/src/services/api/Sensors.ts
@@ -3,5 +3,7 @@ import { get } from "./Base.ts";
 
 export const Sensors = {
     list: (params: AxiosRequestConfig) =>
-        get('/sensors/get-temperature/', params)
+        get('/sensors/get-temperature/', params), 
+    video: (params: AxiosRequestConfig) =>
+        get('/sensors/video-stream', params),
 }
